449a3149bf71f1a845ce2946bd4f4bbc13761a56
slind12s <sarah.linden@gmx.net>
2020-11-10 23:12:14 +0100

    Client Klasse mit der Methode display

--- src/org/hbrs/se/ws20/uebung1/view/Client.java
+++ src/org/hbrs/se/ws20/uebung1/view/Client.java
@@ -1,20 +1,31 @@
 package org.hbrs.se.ws20.uebung1.view;
 
+import org.hbrs.se.ws20.uebung1.control.GermanTranslator;
+
 class Client {
+	public static void main (String [] args){
+		Client u = new Client();
+		u.display(20);
+		u.display(10);
+		u.display(-10);
+	}
 
 	/*
 	 * Methode zur Ausgabe einer Zahl auf der Console
 	 */
 	public void display( int aNumber ){
+
 		// In dieser Methode soll die Methode translateNumber 
 		// mit dem übergegebenen Wert der Variable aNumber 
 		// aufgerufen werden.
 		// Strenge Implementierung gegen das Interface Translator gewuenscht!
-
+		GermanTranslator translators = Hilfsklasse.hilfsmethode();
 		System.out.println("Das Ergebnis der Berechnung: " +
-				"[das Ergebnis an dieser Stelle]" );
+				translators.translateNumber(aNumber));
 
 	}
+
+
 }
 
 
