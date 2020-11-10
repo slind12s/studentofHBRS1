8fd4352070efe6c7f6f3e82d0a48ebfef9276f30
slind12s <sarah.linden@gmx.net>
2020-11-10 23:10:37 +0100

    GermanTranslator Klasse angelegt mit erweiterter Methode translateNumber

--- src/org/hbrs/se/ws20/uebung1/control/GermanTranslator.java
+++ src/org/hbrs/se/ws20/uebung1/control/GermanTranslator.java
@@ -1,5 +1,5 @@
 package org.hbrs.se.ws20.uebung1.control;
-
+import java.util.*;
 public class GermanTranslator implements Translator {
 
 	public String date = "Okt/2020"; // Default-Wert
@@ -7,9 +7,25 @@ public class GermanTranslator implements Translator {
 	/**
 	 * Methode zur Übersetzung einer Zahl in eine String-Repraesentation
 	 */
-	public String translateNumber( int number ) { 
-		// [ihr Source Code aus Übung 1-2] 
-		return null;
+	public String translateNumber( int number ) {
+		// [ihr Source Code aus Übung 1-2]
+		Hashtable<Integer, String> my_dict = new Hashtable<Integer, String>();
+		my_dict.put(1,"eins");
+		my_dict.put(2,"zwei");
+		my_dict.put(3,"drei");
+		my_dict.put(4,"vier");
+		my_dict.put(5,"fünf");
+		my_dict.put(6,"sechs");
+		my_dict.put(7,"sieben");
+		my_dict.put(8,"acht");
+		my_dict.put(9,"neun");
+		my_dict.put(10,"zehn");
+
+		if(number < 1 | number >10){
+			String fehler = "Übersetzung der Zahl " + number + " nicht möglich " + version;
+			return fehler;
+		}
+		return my_dict.get(number);
 	}
 		
 	/**
