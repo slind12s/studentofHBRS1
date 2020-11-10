a38a7a576b40e962becba5eb94fa3d8a8be7ce6d
slind12s <sarah.linden@gmx.net>
2020-11-10 23:57:07 +0100

    Hilfsklasse angelegt zum instanziieren von einem GermanTranslator Object

---
+++ src/org/hbrs/se/ws20/uebung1/view/Hilfsklasse.java
@@ -0,0 +1,12 @@
+package org.hbrs.se.ws20.uebung1.view;
+
+import org.hbrs.se.ws20.uebung1.control.GermanTranslator;
+
+public class Hilfsklasse {
+    public static GermanTranslator hilfsmethode (){
+        GermanTranslator translators = new GermanTranslator();
+        return translators;
+
+
+    }
+}
