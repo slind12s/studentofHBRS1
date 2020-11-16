d37d7f23c1128d4dcdf6563d52f1399ac05c8722
slind12s <sarah.linden@gmx.net>
2020-11-16 14:45:18 +0100

    Anlegen der Methode size, um die Größe der Liste zurückzugeben

--- src/org/hbrs/se/ws20/uebung2/Container.java
+++ src/org/hbrs/se/ws20/uebung2/Container.java
@@ -5,14 +5,17 @@ import java.util.ArrayList;
 public class Container implements Member {
     ArrayList <Integer> list = new ArrayList<>();
     int size = 0;
+    Member member;
 
-    public void addMember(Member member) throws ContainerException {
+    public void addMember(Member member1) throws ContainerException {
+        member = member1;
         for( int aktuellePos = 0; aktuellePos <= size; ++aktuellePos){
             if(member.getID().equals(list.get(aktuellePos))){
                 throw new ContainerException("Das Member-Objekt mit der ID " + member.getID() + "ist bereits vorhanden!");
             }
         }
         list.add(member.getID());
+        ++size;
     }
     public String deleteMember (Integer id){
         for( int aktuellePos = 0; aktuellePos <= size; ++aktuellePos){
@@ -26,7 +29,17 @@ public class Container implements Member {
         return "Die ID wurde erfolgreich aus der Liste entfernt";
     }
     public void dump(){
+        String Id = toString(member.getID());
 
+
+    }
+
+    private String toString(Integer id) {
+        String AusgabeID = "Member ( ID = " + member.getID() + ")";
+        return AusgabeID;
+    }
+    public int size(){
+        return size;
     }
 
     @Override
