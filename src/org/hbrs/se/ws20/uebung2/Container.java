b581aa725524fa959872df9a02e54e17cae80615
slind12s <sarah.linden@gmx.net>
2020-11-16 13:09:00 +0100

    Anlegen der Klasse Container

---
+++ src/org/hbrs/se/ws20/uebung2/Container.java
@@ -0,0 +1,12 @@
+package org.hbrs.se.ws20.uebung2;
+
+import java.util.ArrayList;
+
+public class Container implements Member {
+
+    @Override
+    public Integer getID() {
+        return null;
+    }
+}
+
