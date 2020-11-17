d316d7c79adae35cc240a62173ba970b310b0978
slind12s <sarah.linden@gmx.net>
2020-11-17 21:41:29 +0100

    Anlegen von dem Konstruktor MemberID zur festlegung der ID

--- src/org/hbrs/se/ws20/uebung2/MemberID.java
+++ src/org/hbrs/se/ws20/uebung2/MemberID.java
@@ -1,18 +1,15 @@
 package org.hbrs.se.ws20.uebung2;
 
-public class MemberID extends Container implements Member {
-    int ID = 0;
-    Member member = new Member() {
-        @Override
-        public Integer getID() {
-            return null;
-        }
-    };
-    public MemberID(){
-        this.ID = ID +1;
-    }
-    public int IDWert(){
-        return ID;
+public class MemberID extends Container  {
+    public static int id;
+
+    public MemberID(int idi){
+        this.schluessel = idi;
     }
 
-}
+
+    }
+
+
+
+
